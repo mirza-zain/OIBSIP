716c7eb71b4575d1b07592bf9cb9c6a8f6e34f6d
Mirza Zain <72970095+mirza-zain@users.noreply.github.com>
2023-07-24 04:07:20 -0700

    Finally error is being resolved
    It took me 2 days literally to find the error inside my code and it was an error of spelling mistake. I had wrote celcius instead of celsius 😐

--- Level 1/Temperature Converter/index.js
+++ Level 1/Temperature Converter/index.js
@@ -1,20 +1,27 @@
 const Button = document.querySelector("#btn");
-Button.addEventListener("click", function () {
-    let tempUnit = document.getElementById("unit").value;
+Button.addEventListener("click", function() {
+    
     let temp = document.getElementById("temp").value;
+    let tempUnit = document.getElementById("unit").value;
     let tempConvert = document.getElementById("convert").value;
     let result = document.getElementById("result");
-
-    if (tempUnit == "celcius" && tempConvert =="farenheit") {
-        result.innerHTML = ((temp * 9) / 5 + 32) + " Farenheit";
-    } else if (tempUnit === "farenheit") {
-        result.innerHTML = ((temp - 32) * 5) / 9 + " Celcius";
-    } else if (tempUnit === "kelvin") {
-        result.innerHTML = ((temp - 273.15) * 9) / 5 + 32 + " Farenheit";
-    } else {
-        result.innerHTML = "Please select a unit";
+ 
+    if(tempUnit == "celsius" && tempConvert == "fahrenheit"){
+        result.value = (temp * 9) / 5 + 32;
     }
-    console.log(tempUnit);
-    console.log(temp);
-    console.log(result);
+    if(tempUnit == "celsius" && tempConvert == "kelvin"){
+        result.value = temp + 273.15;
+    }
+    if(tempUnit == "fahrenheit" && tempConvert == "celsius"){
+        result.value = (temp - 32) * 5 / 9;
+    }
+    if(tempUnit == "fahrenheit" && tempConvert == "kelvin"){
+        result.value = (temp - 32) * 5 / 9 + 273.15;
+    }
+    if(tempUnit == "kelvin" && tempConvert == "celsius"){
+        result.value = temp - 273.15;
+    }
+    if(tempUnit == "kelvin" && tempConvert == "fahrenheit"){
+        result.value = (temp - 273.15) * 9 / 5 + 32;
+    }
 });
